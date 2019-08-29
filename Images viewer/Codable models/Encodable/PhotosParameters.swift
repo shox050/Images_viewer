c113491c0dbec22f4af131f5b2ac6b1be1483052
shox050 <shox050@gmail.com>
2019-08-29 07:42:06 +0300

    prepared a network stack without decodable model

---
+++ Images viewer/Codable models/Encodable/PhotosParameters.swift
@@ -0,0 +1,19 @@
+//
+//  PhotosParameters.swift
+//  Images viewer
+//
+//  Created by Vladimir on 29/08/2019.
+//  Copyright © 2019 VladimirYakutin. All rights reserved.
+//
+
+import Foundation
+
+struct PhotosParameters: Encodable {
+    let page: Int
+    let itemsCount: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case page
+        case itemsCount = "per_page"
+    }
+}
---
+++ Images viewer/NetworkService/Implementation/NetworkServiceConfiguration.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkServiceConfiguration.swift
+//  Images viewer
+//
+//  Created by Vladimir on 29/08/2019.
+//  Copyright © 2019 VladimirYakutin. All rights reserved.
+//
+
+import Foundation
+
+class NetworkServiceConfiguration {
+    static let accessKey = "7f146ff75e8b4cebadbe15bc92aa3ad06a7d558c03f2d305d40e175acbda5d11"
+}
